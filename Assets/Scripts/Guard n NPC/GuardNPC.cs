bfca8276507b99a50cfa0734fb54a11f5cb950fd
MrPringleGames <90903487+MrPringleGames@users.noreply.github.com>
2023-03-01 15:42:54 +0000

    Guard and NPC scripting finsihed yay

---
+++ Assets/Scripts/Guard n NPC/GuardNPC.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class GuardNPC : MonoBehaviour
+{
+    // Declare variables
+    public float suspicionLevel;
+    public float fieldOfView;
+    public Transform target;
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    public bool patrolling;
+    public int currentWaypoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        suspicionLevel = 0f;
+        fieldOfView = 180f;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        patrolling = true;
+        currentWaypoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InSight())
+        {
+            suspicionLevel += 0.1f;
+            if (suspicionLevel >= 1)
+            {
+                FollowTarget();
+                patrolling = false;
+            }
+        }
+        else
+        {
+            suspicionLevel = 0f;
+            patrolling = true;
+        }
+
+/*        // Patrol if the NPC is not following the target using NavMesh
+        if (navMeshAgent.destination == null && patrolling == true)
+        {
+            // Move to the next waypoint
+            navMeshAgent.destination = waypoints[currentWaypoint].position;
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+        }
+*/
+
+        // Patrol if the NPC is not following the target using Linear Interpolation (Lerp)
+        if (patrolling == true && !navMeshAgent.pathPending)
+        {
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            {
+                // Lerp (Linear interpolation) to the next waypoint
+                currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+                navMeshAgent.SetDestination(waypoints[currentWaypoint].position);
+            }
+        }
+    }
+
+    // Check if the target is in sight
+    bool InSight()
+    {
+        // Calculate the angle between the target and the NPC
+        Vector3 direction = target.position - transform.position;
+        float angle = Vector3.Angle(direction, transform.forward);
+
+
+        // Check if the target is within the field of view
+        if (angle < fieldOfView * 0.5f)
+        {
+            // Check if there is an obstacle between the target and the NPC
+            Vector3 directionUp = target.position - (transform.position + transform.up);
+            float angleUp = Vector3.Angle(directionUp, transform.forward);
+            Vector3 directionDown = target.position - (transform.position - transform.up);
+            float angleDown = Vector3.Angle(directionDown, transform.forward);
+
+            // Check the angles in all three directions
+            if (angle < fieldOfView * 0.5f && angleUp < fieldOfView * 0.5f && angleDown < fieldOfView * 0.5f)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, direction.normalized, out hit, fieldOfView))
+                {
+                    if (hit.transform == target)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+
+    }
+    // Follow the target
+    void FollowTarget()
+    {
+        // NavMeshAgent to follow target while avoiding obstacles
+        NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.destination = target.position;
+    }
+
+}
+
---
+++ Assets/Scripts/Guard n NPC/NPC.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.AI;
+
+
+public class NPC : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent navMeshAgent;
+    public float wanderRadius;
+
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (navMeshAgent.velocity == Vector3.zero)
+        {
+            // Generate a random direction
+            Vector3 newDirection = Random.insideUnitSphere * wanderRadius;
+            newDirection += transform.position;
+            UnityEngine.AI.NavMeshHit hit;
+            UnityEngine.AI.NavMesh.SamplePosition(newDirection, out hit, wanderRadius, 1);
+            Vector3 finalPosition = hit.position;
+            navMeshAgent.SetDestination(finalPosition);
+        }
+    }
+
+}
